692142a85f530e77475d17cc324f67a23fe9a1d9
Ian Page Maciel <ianpage27@gmail.com>
2025-03-10 17:01:26 -0300

    mais sobre mim quase completo

--- PortifolioC/src/components/mais_sobre/index.jsx
+++ PortifolioC/src/components/mais_sobre/index.jsx
@@ -5,23 +5,23 @@ const MaisSObreMim = () =>{
     return(
         <>
         <div id='mais-sobre-mim'>
-            <div className='container'>
-                <h1>Mais sobre mim</h1>
-                <div className='conteudo_sobre'>
-                        <h2>SOBRE</h2>
-                            <p>Acredito que um bom front-end é essencial para o sucesso de qualquer projeto digital. Por isso, me dedico a criar soluções de alta qualidade.</p>
-                            <div>
+            <div className='container d-flex justify-content-between align-items-center'>
+                
+                    <div className='conteudo_sobre'>
+                        <h2> MAIS SOBRE</h2>
+                        <p>Acredito que um bom front-end é essencial para o sucesso de qualquer projeto digital. Por isso, me dedico a criar soluções de alta qualidade. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore vero vitae doloribus! Magni dicta, ut doloremque veniam laudantium nesciunt, sint quia repellat esse ratione expedita quos nisi? Quibusdam, distinctio quas?</p>
+                        <div>
                             <div className='btn_sobre'>
-                                <a href="#">Projetos</a>
+                                <a href="#">Repositorio</a>
                             </div>
-                                <div className='btn_sobre'>
-                                    <a href="#">Entre em Contato</a>
-                                </div>
-                                </div>
-                                </div>
-                                    <div>
-                                    <img src=""  alt=""  className='img_sobre'/>
-                                    </div>
+                            <div className='btn_sobre'>
+                                <a href="#">Curriculo</a>
+                            </div>
+                        </div>
+                    </div>
+                        <div>
+                            <img src="https://placehold.co/300x300"  alt=""  className='img_sobre'/>
+                        </div>
             </div>
         </div>
         </>
