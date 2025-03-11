0e2b7aa564f653c5afc33b85daf0d785e23a911d
Ian Page Maciel <ianpage27@gmail.com>
2025-03-10 21:30:46 -0300

    media 768

--- PortifolioC/src/components/about/index.jsx
+++ PortifolioC/src/components/about/index.jsx
@@ -6,7 +6,7 @@ function About() {
     return (
         <>
             <section id='about'>
-                <div className='d-flex justify-content-between align-items-center container'>
+                <div className='d-flex justify-content-between align-items-center container w-768'>
                     <div className='conteudo_sobre'>
                         <h2>SOBRE</h2>
                         <p>Acredito que um bom front-end é essencial para o sucesso de qualquer projeto digital. Por isso, me dedico a criar soluções de alta qualidade.</p>
